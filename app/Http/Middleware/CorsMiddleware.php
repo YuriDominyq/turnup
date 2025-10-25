1389f75f2fae3c12ff96a2d27ce3a135f9bc1e4a
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-10-25 11:43:32 +0800

    Updated Backend

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,7 +15,7 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-         $allowedOrigin = 'http://127.0.0.1:5173';
+         $allowedOrigin = 'https://turnup-main-17rtje.laravel.cloud';
    
         if ($request->getMethod() === 'OPTIONS') {
             return response()->noContent(204)
