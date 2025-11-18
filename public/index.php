f788361771ee234824b3e979f7ae4cda8f953736
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-11-18 15:10:01 +0800

    Updated Website

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
-header("Cache-Control: public, max-age=3600, must-revalidate");
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
