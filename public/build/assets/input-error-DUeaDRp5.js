043d003374303015eeff26125ee3b2f170c39405
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-11-21 20:52:44 +0800

    Update frontend production build

---
+++ public/build/assets/input-error-DUeaDRp5.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CE1c2oiz.js";import{c as o}from"./app-logo-icon-BI_dkTTI.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
