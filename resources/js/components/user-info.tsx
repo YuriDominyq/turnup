81eb1c8f8c090e8aafb66014ba9fa301ec5a6f7b
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-11-25 20:32:27 +0800

    Just Post a Photo

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -8,7 +8,7 @@ export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: 
     return (
         <>
             <Avatar className="h-8 w-8 overflow-hidden rounded-full">
-                <AvatarImage src={user.photo_url ?? undefined} alt={`${user.first_name} ${user.last_name}`} />
+                <AvatarImage src={user.full_photo_url ?? undefined} alt={`${user.first_name} ${user.last_name}`} />
                 <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                     {getInitials(user.first_name, user.last_name)}
                 </AvatarFallback>
