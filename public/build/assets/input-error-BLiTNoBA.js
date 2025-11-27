ac4071feff9d8ed1d39ae3d998c96f4829e7c85a
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-11-27 17:11:34 +0800

    Update frontend production build

--- public/build/assets/input-error-BLiTNoBA.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CG_ak2UX.js";import{c as o}from"./app-logo-icon-DA02lGm0.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
