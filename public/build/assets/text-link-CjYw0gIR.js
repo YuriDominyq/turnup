bc9aa6cfaf0feca1f86ca06290c8ba7a45b89ae8
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-11-27 16:11:30 +0800

    Update frontend production build

--- public/build/assets/text-link-CjYw0gIR.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as n}from"./app-B2N4sAYZ.js";import{c as a}from"./app-logo-icon-CY43iG4N.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
