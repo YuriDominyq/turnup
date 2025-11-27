fbdf2050b01c5c123e3eede832c507c7e2ac0497
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-11-27 17:18:42 +0800

    Update frontend production build

--- public/build/assets/input-error-nhTTIUJs.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DAeZ2y_k.js";import{c as o}from"./app-logo-icon-DOgZtAXV.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
