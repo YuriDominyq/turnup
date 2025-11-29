508d35b82b191673e7be0083428f3c538458ba69
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-11-29 20:30:10 +0800

    Just Update KPI for Commuters

--- resources/js/components/admin/dashboard-kpi.tsx
+++ resources/js/components/admin/dashboard-kpi.tsx
@@ -30,7 +30,7 @@ export default function DashboardKPI() {
     return (
         <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4'>
             <KPICard
-                title="Total Users"
+                title="Total Commuters"
                 value={kpiData.totalUsers}
                 icon={Users}
                 progress={progressUsers}
