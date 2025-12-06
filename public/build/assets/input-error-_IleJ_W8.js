0ee40e744ee68fb05d1013a58ad023e42d281dd1
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-12-06 08:27:45 +0800

    Update frontend production build

--- public/build/assets/input-error-_IleJ_W8.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-C06O5J4D.js";import{c as o}from"./app-logo-icon-DuvCb4eO.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
