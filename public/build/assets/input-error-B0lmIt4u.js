a626ea76da9a254eaa4637365ae55e9b32409e1c
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-12-06 17:58:39 +0800

    Update frontend production build

--- public/build/assets/input-error-B0lmIt4u.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CqX-_z8s.js";import{c as o}from"./app-logo-icon-BDKbC3yN.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
