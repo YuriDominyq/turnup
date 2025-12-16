046748891670365fe58442c225049b48acef8b52
YuriDominyq <137363317+YuriDominyq@users.noreply.github.com>
2025-12-16 18:19:22 +0800

    Update frontend production build

--- public/build/assets/input-error-BetEaDJj.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-JvA9W9jJ.js";import{c as o}from"./app-logo-icon-qln48QBh.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
